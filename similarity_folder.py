99062951be692d1d31a55fcefffe97d0c1492a0c
TheGwAck <wback57@gmail.com>
2022-07-15 14:20:30 -0400

    whole folder similarity

--- similarity_folder.py
+++ similarity_folder.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
-import sys
 
 
 # The demos are ordered so as to make the explanations in the comments consistent. If you only 
@@ -24,25 +23,16 @@ encoder = VoiceEncoder()
 # We'll use a smaller version of the dataset LibriSpeech test-other to run our examples. This 
 # smaller dataset contains 10 speakers with 10 utterances each. N.B. "wav" in variable names stands
 # for "waveform" and not the wav file extension.
-# wav_fpaths = list(Path("audio_data", "librispeech_test-other").glob("**/*.flac"))
+path = '/content/drive/MyDrive/Collabera_Jawad/test_audio'
+wav_fpaths = list(Path(path).glob("*.wav"))
+print(wav_fpaths)
+print(type(wav_fpaths))
 # Group the wavs per speaker and load them using the preprocessing function provided with 
 # resemblyzer to load wavs in memory. It normalizes the volume, trims long silences and resamples 
 # the wav to the correct sampling rate.
-
-path = '/content/drive/MyDrive/Collabera_Jawad/myaudio'
-
-speaker_1 = sys.argv[1]
-speaker_2 = sys.argv[2]
-
-
-wav_1_fpaths = list(Path(path, speaker_1).glob("*.wav"))
-wav_2_fpaths = list(Path(path, speaker_2).glob("*.wav"))
-
-
-speaker_1_wavs = {speaker_1: list(map(preprocess_wav, wav_1_fpaths))}
-speaker_2_wavs = {speaker_2: list(map(preprocess_wav, wav_2_fpaths))}
-speaker_1_wavs.update(speaker_2_wavs) 
-speaker_wavs = speaker_1_wavs
+speaker_wavs = {speaker: list(map(preprocess_wav, wav_fpaths)) for speaker, wav_fpaths in
+                groupby(tqdm(wav_fpaths, "Preprocessing wavs", len(wav_fpaths), unit="wavs"), 
+                        lambda wav_fpath: wav_fpath.parent.stem)}
 
 
 ## Similarity between two utterances from each speaker
@@ -81,7 +71,7 @@ spk_sim_matrix = np.inner(spk_embeds_a, spk_embeds_b)
 fix, axs = plt.subplots(2, 2, figsize=(8, 10))
 labels_a = ["%s-A" % i for i in speaker_wavs.keys()]
 labels_b = ["%s-B" % i for i in speaker_wavs.keys()]
-mask = np.eye(len(utt_sim_matrix), dtype=np.bool_)
+mask = np.eye(len(utt_sim_matrix), dtype=np.bool)
 plot_similarity_matrix(utt_sim_matrix, labels_a, labels_b, axs[0, 0],
                        "Cross-similarity between utterances\n(speaker_id-utterance_group)")
 plot_histograms((utt_sim_matrix[mask], utt_sim_matrix[np.logical_not(mask)]), axs[0, 1],
