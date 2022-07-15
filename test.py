d946f1392e260a8bd6c4866f41eb0deff38be3ea
TheGwAck <wback57@gmail.com>
2022-07-15 15:03:15 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
 from resemblyzer import VoiceEncoder, preprocess_wav
 from pathlib import Path
+import sys
+import numpy as np
 
-fpath = Path("path_to_an_audio_file")
-wav = preprocess_wav(fpath)
+path_1 = sys.argv[1]
+path_2 = sys.argv[2]
+
+fpath_1 = Path(path_1)
+wav_1 = preprocess_wav(fpath_1)
+fpath_2 = Path(path_2)
+wav_2 = preprocess_wav(fpath_2)
 
 encoder = VoiceEncoder()
-embed = encoder.embed_utterance(wav)
+embed_1 = encoder.embed_utterance(wav_1)
+embed_2 = encoder.embed_utterance(wav_2)
+
 np.set_printoptions(precision=3, suppress=True)
 print(embed)
