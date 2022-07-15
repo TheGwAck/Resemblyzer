c6a2028d0fb55fa1a58d50c9863a806074f2aaa7
TheGwAck <wback57@gmail.com>
2022-07-15 14:30:52 -0400

    debug

--- similarity_folder.py
+++ similarity_folder.py
@@ -31,9 +31,8 @@ wav_fpaths = list(Path(path).glob("**/*.wav"))
 # the wav to the correct sampling rate.
 speaker_wavs = {speaker: list(map(preprocess_wav, wav_fpaths)) for speaker, wav_fpaths in
                 groupby(tqdm(wav_fpaths, "Preprocessing wavs", len(wav_fpaths), unit="wavs"), 
-                        lambda wav_fpath: print(wav_fpath.parent.stem))}
-for wavs in speaker_wavs.values():
-    print(wavs)
+                        lambda wav_fpath: wav_fpath.parent.stem)}
+print(speaker_wavs)
 
 ## Similarity between two utterances from each speaker
 # Embed two utterances A and B for each speaker
